15bfd75940f86064c2d461d1020ec33a42af93fd
shubham-sandanshiv <ssandanshiv05@gmail.com>
2024-10-04 13:22:37 +0530

    Header

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -5,62 +5,53 @@ const Navbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-blue-500 p-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <div className="text-white text-2xl font-bold">
-          <Link to="/">{props.title}</Link>
+    <header class="header" id="header">
+      <nav class="nav container">
+        <a href="#" class="nav__logo">Marlon</a>
+
+        <div class="nav__menu" id="nav-menu">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a href="#home" class="nav__link active-link">
+                <i class='bx bx-home-alt nav__icon'></i>
+                <span class="nav__name">Home</span>
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#about" class="nav__link">
+                <i class='bx bx-user nav__icon'></i>
+                <span class="nav__name">About</span>
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#skills" class="nav__link">
+                <i class='bx bx-book-alt nav__icon'></i>
+                <span class="nav__name">Skills</span>
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#portfolio" class="nav__link">
+                <i class='bx bx-briefcase-alt nav__icon'></i>
+                <span class="nav__name">Portfolio</span>
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#contactme" class="nav__link">
+                <i class='bx bx-message-square-detail nav__icon'></i>
+                <span class="nav__name">Contactme</span>
+              </a>
+            </li>
+          </ul>
         </div>
-        <div className="md:hidden">
-          <button onClick={() => setIsOpen(!isOpen)} className="text-white">
-            {isOpen ? (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            ) : (
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
-              </svg>
-            )}
-          </button>
-        </div>
-        <div className={`md:flex items-center space-x-4 ${isOpen ? "block" : "hidden"} md:block`}>
-          <Link to="/" className="text-white hover:bg-blue-700 p-2 rounded">
-            Home
-          </Link>
-          <Link to="/dashboard" className="text-white hover:bg-blue-700 p-2 rounded">
-            Dashboard
-          </Link>
-          <Link to="/login" className="text-white hover:bg-blue-700 p-2 rounded">
-            Login
-          </Link>
-          <Link to="/signup" className="text-white hover:bg-blue-700 p-2 rounded">
-            Signup
-          </Link>
-        </div>
-      </div>
-    </nav>
+
+        <img src="assets/img/perfil.png" alt="" class="nav__img"/>
+      </nav>
+    </header>
+
   );
 };
 
